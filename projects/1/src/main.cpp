aafb93880840be1ec68d88058b2f99d26c66a79d
unnar19 <unnar19@ru.is>
2023-09-06 13:17:04 +0000

    add serial

--- projects/1/src/main.cpp
+++ projects/1/src/main.cpp
@@ -10,15 +10,20 @@ int position;
 // ISR (INT0_vect);
 
 int main(){
+  Serial.begin(115200);
   double PPS_encoder = ((155/100)*1400)/60; // Pulses per sec
   double sleep_time_ms = (1/PPS_encoder)/1000;
   Motor_Encoder enc(3, 4); // D3 and D4
   enc.init();
 
+  Digital_out led(5);
+  led.init();
+
   while(1){
     unsigned long start_time = millis();
 
-    while(millis() - start_time > 5000){ 
+    while(millis() - start_time > 500){ 
+      led.set_lo();
       unsigned long last_run = millis();
       if (millis() - last_run > sleep_time_ms){
         last_run = millis();
@@ -28,10 +33,8 @@ int main(){
       Serial.print("Current position count: ");
       Serial.print(position);
     }
-
-    Digital_out led(5);
-    led.init();
-
+    _delay_ms(1000);
+    led.set_hi();
   }
 
   return 0;
